27e564f2d14a4c827608fef19d7c2a9daa0d9c7a
inho.kim <inho.kim@softwareinlife.com>
2022-04-19 20:05:03 +0900

    Initial commit

---
+++ src/main/java/com/ino/SpringbootGcpMonorepoApplication.java
@@ -0,0 +1,13 @@
+package com.ino;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootGcpMonorepoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootGcpMonorepoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ino/SpringbootGcpMonorepoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ino;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootGcpMonorepoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
